03917dd3f68aa8b9e7bbf854e05c64af04b4e91b
Tom Maher <tmaher@github.com>
2017-03-27 19:01:01 -0700

    split up ldap

--- spec/vault_provision_spec.rb
+++ spec/vault_provision_spec.rb
@@ -10,6 +10,14 @@ describe Vault::Provision do
     expect(client.sys.auths[:ldap].type).to be == 'ldap'
   end
 
+  it "has a token auth" do
+    expect(client.sys.auths[:token].type).to be == 'token'
+  end
+
+  it "has an ldap config" do
+    client.get('auth/ldap/config').to be
+  end
+
   #it "has a pki-root mount" do
   #  expect(client.sys.mounts).to include 'pki-root'
   #end
